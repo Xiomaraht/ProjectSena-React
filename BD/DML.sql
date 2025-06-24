e198d79f83e18fcccfa4471bd2ab3c1d645411b9
Tronywhat <tomasfelipelagunaacevedo@gmail.com>
2025-06-24 00:26:31 -0500

    lista de tablas normales

--- BD/DML.sql
+++ BD/DML.sql
@@ -78,3 +78,61 @@ INSERT INTO Suscripcion(idSuscripcion, nombre, descripcion, costoMensual, estado
 (3, 'Plan Premium', 'Cobertura completa incluyendo exámenes y descuentos en cirugías.', '150000', 'Activo'),
 (4, 'Plan Cachorros', 'Plan especial para el primer año de vida de la mascota.', '90000', 'Activo'),
 (5, 'Plan Geriátrico', 'Cuidado especializado para mascotas mayores de 7 años.', '110000', 'Activo');
+
+-- 10. Tabla: Citas
+INSERT INTO Citas(fechaYhora, clientesNumDo, clientesTipDoc, empleadosNumDo, empleadosTipDoc) VALUES
+("24/06/2025-10:00", '1016595990', 'Cédula de Ciudadanía', '5959901016', 'Cédula de Extranjería'),
+("24/07/2025-10:00", '1016595991', 'Cédula de Extranjería', '4959901016', 'Pasaporte'),
+("24/08/2025-10:00", '1016595992', 'Pasaporte', '3959901016', 'Cédula de Ciudadanía'),
+("24/09/2025-10:00", '1016595993', 'Permiso Especial de Permanencia', '2959901016', 'Cédula de Extranjería'),
+("24/10/2025-10:00", '1016595994', 'Cédula de Extranjería', '1959901016', 'Permiso Especial de Permanencia');
+
+-- 11. Tabla: Consultas
+INSERT INTO Consultas(fecCon, reporConsult, resultExam, empleadosNumDoc, empleadosTipDoc, idMasc) VALUES
+("24/06/2025-10:00", '1016595990', 'Cédula de Ciudadanía', '5959901016', 'Cédula de Extranjería',1),
+("24/07/2025-10:00", '1016595991', 'Cédula de Extranjería', '4959901016', 'Pasaporte',2),
+("24/08/2025-10:00", '1016595992', 'Pasaporte', '3959901016', 'Cédula de Ciudadanía',3),
+("24/09/2025-10:00", '1016595993', 'Permiso Especial de Permanencia', '2959901016', 'Cédula de Extranjería',4),
+("24/10/2025-10:00", '1016595994', 'Cédula de Extranjería', '1959901016', 'Permiso Especial de Permanencia',5);
+ -- report consulta y resultado examen faltan
+ 
+ -- 12. Tabla: Inventario
+INSERT INTO Inventario(productoID, cantidadActual, fechaUltimaActualizacion, veterinariaNit) VALUES
+(1, 10, "24/06/2025-10:00", '595990101'),
+(2, 16, "24/06/2025-10:00", '495990101'),
+(3, 11, "24/06/2025-10:00", '395990101'),
+(4, 17, "24/06/2025-10:00", '295990101'),
+(5, 12, "24/06/2025-10:00", '195990101');
+
+ -- 13. Tabla: Producto
+INSERT INTO Producto(image, precio, stock, idInventario) VALUES
+("image_1", 10000, 10, 1),
+("image_2", 16000, 23, 2),
+("image_3", 11000, 16, 3),
+("image_4", 17000, 11, 4),
+("image_5", 1200, 27, 5);
+
+ -- 14. Tabla: suscripciónesVeterinarias
+INSERT INTO suscripciónesVeterinarias(veterinariaNit, idSuscripcion, fechaInicio, fechaFin, estadoSuscripcion, fechaUltimoPago, proximoPago) VALUES
+("595990101", 1, "24/06/2025", "24/07/2025","Activo","24/05/2025","24/01/2025"),
+("495990101", 2, "24/06/2025", "24/07/2025","Cancelada","24/05/2025","24/01/2025"),
+("395990101", 3, "24/06/2025", "24/07/2025","Activo","24/05/2025","24/01/2025"),
+("295990101", 4, "24/06/2025", "24/07/2025","Activo","24/05/2025","24/01/2025"),
+("195990101", 5, "24/06/2025", "24/07/2025","Cancelada","24/05/2025","24/01/2025");
+
+ -- 15. Tabla: PagosSuscripcion
+INSERT INTO PagosSuscripcion(idSuscripcionVeterinaria, montoPagado, fechaPagado, peridoFacturadoInicio, periodoFacturadoFin, metodoPago, estadoPago, referenciaTransaccion) VALUES
+(1, 15000, "24/06/2025", "24/07/2025", "24/08/2025","Efectivo","recibido","Efectivo"),
+(2, 20000, "24/06/2025", "24/07/2025", "24/08/2025","Tarjeta débito o crédito","cancelado","M1708252"),
+(3, 22000, "24/06/2025", "24/07/2025", "24/08/2025","Transferencias bancarias","cancelado","824139512"),
+(4, 17500, "24/06/2025", "24/07/2025", "24/08/2025","Efectivo","recibido","Efectivo"),
+(5, 24000, "24/06/2025", "24/07/2025", "24/08/2025","Efectivo","recibido","Efectivo");
+
+ -- 15. Tabla: Facturas
+INSERT INTO Facturas(fechaFactura, fechaVencimiento, subtotal, impuestos, totalFactura) VALUES
+("24/06/2025", "24/10/2025", 15000, 2850, 17850),
+("24/06/2025", "24/10/2025", 20000, 3800, 23800),
+("24/06/2025", "24/10/2025", 22500, 4275, 26775),
+("24/06/2025", "24/10/2025", 11250, 2137, 13387),
+("24/06/2025", "24/10/2025", 5500, 1045, 6545);
+
