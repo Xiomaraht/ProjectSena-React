b32a5921662b6cbc04d924ebc0380f0dfc97a699
Lina Wilches <lpw151205@gmail.com>
2025-06-26 16:29:03 -0500

    Ajustes para Manuel (Señor Zudro)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdmiClinicoVetLg from './AdmiClinicoVetLg'
 import LandingPageMq from './LandingPageMq'
 import Login from './Login'
-import Products from './Products'
+import ProductsLw from './ProductsLw'
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
         <Route path='/' element={<LandingPageMq/>}/>
         <Route path='/home' element={<AdmiClinicoVetLg/>}/>
         <Route path='/login' element={<Login />}/>
-        <Route path='/products' element={<Products />}/>
+        <Route path='/products' element={<ProductsLw />}/>
       </Routes>
     </BrowserRouter>
     </>
